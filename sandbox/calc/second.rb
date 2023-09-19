3fde87c7d6508f92078b835a765aae22407bf3f5
Christian <christian.muehlethaler@optor.ch>
2023-09-19 15:09:06 +0200

    Kleine Änderungen im 2. Beispiel

--- sandbox/calc/second.rb
+++ sandbox/calc/second.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/ebnf'
-puts
 File.dirname(__FILE__)
 grammar = EBNF.parse(File.open(__dir__+"/calc.ebnf"))
-peg = grammar.make_peg
-ruby_code = grammar.to_ruby
 
-puts "-----"
-puts peg
-puts "-----"
-puts ruby_code
-puts "-----"
-
-
-puts grammar.build_tables
+puts "sxp:-----"
+puts grammar.to_sxp
+puts "Ruby Code: -----"
 puts grammar.to_ruby
+puts "string -----"
+puts grammar.to_s
+
+
+#puts grammar.build_tables
+#puts grammar.to_ruby
